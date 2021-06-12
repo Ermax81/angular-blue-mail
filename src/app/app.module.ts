0c23cd022b0d2995286cd3e88e3deb48ad86f625
Ermax81 <enozay.dev@gmail.com>
2021-06-12 12:04:05 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from './material.module';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {TabEmailComponent} from "./tab-email/tab-email.component";
+import {EmailService} from "./email.service";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabEmailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    AppRoutingModule,
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [
+    EmailService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
